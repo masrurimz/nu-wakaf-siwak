79b8ed900b51ed3e80af7fc0eb14b11838383f17
Muhammad Zahid Masruri <zahid@hellonimbly.com>
2022-11-26 10:08:01 +0700

    Setup dotenv

---
+++ src/lib/wakaf/WakafForm/WakafForm.screen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const WakafFormScreen = () => {
+  return (
+    <View>
+      <Text>WakafFormScreen</Text>
+    </View>
+  );
+};
+
+export default WakafFormScreen;
