3d19192d4666c7239899d93dddc0489a798eaa38
Muhammad Zahid Masruri <zahid@hellonimbly.com>
2022-11-26 11:25:08 +0700

    Fix dotenv

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,17 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      [
-        'module:react-native-dotenv',
-        {
-          envName: 'APP_ENV',
-          moduleName: '@env',
-          path: '.env',
-          blocklist: null,
-          allowlist: null,
-          safe: false,
-          allowUndefined: false,
-          verbose: false,
-        },
-      ],
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: false,
+        verbose: false,
+      },
     ],
   ],
 };
--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -1,3 +1,4 @@
 declare module '@env' {
   export const API_BASE: string;
+  export const IS_STORYBOOK_MODE: boolean;
 }
