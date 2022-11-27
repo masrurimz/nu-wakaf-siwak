40fdfad0dd7ddb405d72d49684870a0662516b6a
Muhammad Zahid Masruri <zahid@hellonimbly.com>
2022-11-27 10:17:08 +0700

    Fix storybook loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.?(ts|tsx|js|jsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-react-native-web',
-  ],
-  framework: '@storybook/react',
+  addons: [],
 };
